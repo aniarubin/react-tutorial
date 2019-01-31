3993cf4b36a90fc25c14b47a37f00f6310cda54d
aniarubin <anialeguin@gmail.com>
2019-01-31 15:37:24 +0400

    Updates

---
+++ precache-manifest.a0c2c9d67fbf619e83330c5030ab8339.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6d0aa42199878ab9859",
+    "url": "/react-tutorial/static/js/runtime~main.d6d0aa42.js"
+  },
+  {
+    "revision": "efed951a69e40a84521c",
+    "url": "/react-tutorial/static/js/main.efed951a.chunk.js"
+  },
+  {
+    "revision": "f4789f78bc5099d1ff6c",
+    "url": "/react-tutorial/static/js/1.f4789f78.chunk.js"
+  },
+  {
+    "revision": "efed951a69e40a84521c",
+    "url": "/react-tutorial/static/css/main.bcbf2197.chunk.css"
+  },
+  {
+    "revision": "77951cff34500576ab6c27d589613105",
+    "url": "/react-tutorial/index.html"
+  }
+];
